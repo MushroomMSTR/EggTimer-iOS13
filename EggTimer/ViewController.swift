401ab24bff85575ed2bc92c3b1b632b5319a4903
NazarStf <hworker98@gmail.com>
2023-05-22 00:30:27 +0300

    done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    
+	
+	let timers = ["Soft": 300, "Medium": 420, "Hard": 720]
+	
+	var player: AVAudioPlayer?
+	
+	func playSound(soundName: String) {
+		guard let path = Bundle.main.path(forResource: soundName, ofType:"mp3") else {
+			print("Sound file \(soundName) not found")
+			return
+		}
+		let url = URL(fileURLWithPath: path)
+		
+		do {
+			player = try AVAudioPlayer(contentsOf: url)
+			player?.play()
+			
+		} catch let error {
+			print("Error playing sound: \(error.localizedDescription)")
+		}
+	}
+	
+	@IBOutlet weak var progressBar: UIProgressView!
+	@IBOutlet weak var titleDone: UILabel!
+	
+	var timer: Timer?
+	var remainingTime: Int = 0
+	var originalTime: Int = 0
+	
+	@IBAction func hardnessSelected(_ sender: UIButton) {
+		if let hardness = sender.currentTitle, let time = timers[hardness] {
+			startTimer(with: time, hardness: hardness)
+		}
+	}
+	
+	func startTimer(with time: Int, hardness: String) {
+		remainingTime = time
+		originalTime = time
+		progressBar.progress = 0.0
+		timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+		print("Timer started for \(hardness) (\(time) seconds)")
+	}
+	
+	@objc func updateTimer() {
+		if remainingTime > 0 {
+			remainingTime -= 1
+			let progress = Float(originalTime - remainingTime) / Float(originalTime)
+			progressBar.progress = progress
+			print("\(remainingTime) seconds remaining")
+		} else {
+			stopTimer()
+			playSound(soundName: "alarm_sound")
+			titleDone.text = "DONE!"
+			print("Timer ended")
+			
+			DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+				self.titleDone.text = "How do you like your eggs?"
+			}
+		}
+	}
 
-
+	
+	func stopTimer() {
+		timer?.invalidate()
+		timer = nil
+	}
 }
+
+
